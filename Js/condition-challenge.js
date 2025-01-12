a412a437198460e8e7af4f4b7e248acae7944260
Daniel Williams <williamsoligodaniel@gmail.com>
2025-01-12 07:23:46 +0100

    Create condition-challenge.js

---
+++ Js/condition-challenge.js
@@ -0,0 +1,58 @@
+/*
+let num1 = 5;
+let num2 = 8;
+
+if (num1 > num2) {
+    console.log("Yeah, num1 is greater")
+} else {
+    console.log("Nope, num1 is not greater than num2");
+    
+}
+*/
+
+/*
+let username = "Dan";
+let anotherUserName = "Dan";
+
+if (username == anotherUserName) {
+    console.log("Pick another username");
+    
+} else {
+    console.log("Username is available");
+    
+}
+*/
+
+//Checking if a variable is a number or not
+/*
+let score = 44;
+if (typeof score === "number") {
+    console.log("Yes");
+} else {
+    console.log("No");
+    
+}
+*/
+
+//checking if a boolean value is true or false
+/*
+let isTeaReady = false;
+if (isTeaReady) {
+    console.log("Yes");
+} else {
+    console.log("No");
+    
+}
+    */
+
+//Checking if array is empty
+let items = [];
+console.log(items.length);
+
+if (items.length === 0) {
+    console.log("Array is empty");
+    
+} else {
+    console.log("Array is not empty");
+    
+}
